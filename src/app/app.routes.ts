6ebf285f30300284764b4093d0176f10e4bc1e38
Alejandro Bolaños <alex870915@gmail.com>
2023-06-20 08:28:50 -0400

    add nopagefound

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,5 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     { path: 'auth', loadChildren: () => import('./auth/auth.routes') },
-    //{ path: '**', loadComponent: () => import( './nopagefound/nopagefound.component' ).then(({ NopagefoundComponent }) => NopagefoundComponent) },
+    { path: '**', loadComponent: () => import( './nopagefound/nopagefound.component' ).then(({ NopagefoundComponent }) => NopagefoundComponent) },
 ];
---
+++ src/app/nopagefound/nopagefound.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+    selector: 'app-nopagefound',
+    standalone: true,
+    imports: [CommonModule, RouterModule],
+    templateUrl: './nopagefound.component.html',
+    styleUrls: ['./nopagefound.component.sass']
+})
+export class NopagefoundComponent {
+
+}
