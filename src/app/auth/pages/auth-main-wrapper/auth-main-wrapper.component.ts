c2ed7e5e9c055a3867335a9fb3ae003fcf56b925
Alejandro Bolaños <alex870915@gmail.com>
2023-06-20 08:34:18 -0400

    add shared folder and footer component

--- src/app/auth/pages/auth-main-wrapper/auth-main-wrapper.component.ts
+++ src/app/auth/pages/auth-main-wrapper/auth-main-wrapper.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from 'src/app/shared/footer/footer.component';
 
 @Component({
   selector: 'app-auth-main-wrapper',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FooterComponent],
   templateUrl: './auth-main-wrapper.component.html',
   styleUrls: ['./auth-main-wrapper.component.sass']
 })
